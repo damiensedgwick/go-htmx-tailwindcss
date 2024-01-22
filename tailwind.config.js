f8ee7cf4df7f2ecc6eb56426aaff01cdc9c7c710
Damien Sedgwick <damienksedgwick@gmail.com>
2024-01-22 13:18:11 +0000

    removed previous animation attempts, try again

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,8 @@ module.exports = {
     theme: {
         extend: {
             animation: {
-                "pulse": "cubic-bezier(0, 0, 0, 0.5) infinite",
-                "glow": "5s linear infinite",
             },
             keyframes: {
-                glow: {
-                    "0%": {
-                        background: "red"
-                    },
-                    "33%": {
-                        background: "green"
-                    },
-                    "66%": {
-                        background: "blue"
-                    },
-                    "100%": {
-                        background: "red"
-                    },
-                },
-                pulse: {
-                    "50%": {
-                        transform: "scale(2)"
-                    }
-                }
             },
         },
     },
